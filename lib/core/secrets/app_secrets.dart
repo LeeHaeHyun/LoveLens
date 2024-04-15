a72e9b403345344e99450dacf8576ccd8fd00ed8
LeeHaeHyun <137026483+LeeHaeHyun@users.noreply.github.com>
2024-04-16 04:45:05 +0900

    Delete lib/core/secrets directory

--- lib/core/secrets/app_secrets.dart
+++
@@ -1,5 +0,0 @@
-class AppSecrets {
-  static const supabaseUrl = 'https://qpvkkduwpmujruixgagz.supabase.co';
-  static const supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFwdmtrZHV3cG11anJ1aXhnYWd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIwNDg4MzksImV4cCI6MjAyNzYyNDgzOX0.hOlo5Ypk9iPM95rBUsirrNAiCMIrJKO8b9qSv7rfLbc';
-}
